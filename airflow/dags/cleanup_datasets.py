43090ba1083e36fa683dfbf423fe0b936dd82dee
Adam Lowe <data@ash-shahada.org>
2025-08-08 11:50:10 +0100

    latest

--- airflow/dags/cleanup_datasets.py
+++ airflow/dags/cleanup_datasets.py
@@ -10,24 +10,6 @@ server_config = "/home/asha/airflow/duckdb-config.json"
 with open(server_config, "r") as fp:
     config = json.load(fp)
 token = config['token']
-
-# @task
-# def cleanup_yearly_tenant_data():
-#     cleanup_data(
-#      token=token,
-#      bronze_schema='bronze',
-#      bronze_table_name='yearly_tenant_data'
-#     )
-    
-# @task
-# def fuzzy_cleanup_yearly_tenant_data():
-#     fuzzy_group_data(
-#         token=token,
-#         bronze_schema='bronze',
-#         bronze_table_name='yearly_tenant_data',
-#         column='ReferralAgency', 
-#         group_column_name='GroupedReferralAgency'
-#     )
     
 @task
 def cleanup_tenant_data():
@@ -55,20 +37,9 @@ def fuzzy_cleanup_tenant_data():
     tags=["optimisation"]
 )
 def garbage_cleanup():
-    # t1 = cleanup_yearly_tenant_data() 
-    # t2 = fuzzy_cleanup_yearly_tenant_data()
-    # t3 = cleanup_tenant_data() 
-    # t4 = fuzzy_cleanup_tenant_data()
-    
-    # t1 >> t2
-    # t3 >> t4
-    
-    # t1 = cleanup_yearly_tenant_data() 
-    # t2 = fuzzy_cleanup_yearly_tenant_data()
-    t3 = cleanup_tenant_data() 
-    t4 = fuzzy_cleanup_tenant_data()
-    
-    # t1 >> t2
-    t3 >> t4
+    t1 = cleanup_tenant_data() 
+    t2 = fuzzy_cleanup_tenant_data()
+
+    t1 >> t2
    
 dag_instance = garbage_cleanup()
